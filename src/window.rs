cae74d4db0cf7246ce86415e9e961a8520cadab6
StepanTheGreat <117771383+StepanTheGreat@users.noreply.github.com>
2026-02-10 12:17:03 +0100

    Acknowledgment window

--- src/window.rs
+++ src/window.rs
@@ -1,15 +1,5 @@
-use std::fmt::Display;
-
 use crate::packet::PacketSeqId;
 
-
-#[cfg(test)] 
-/// For testing we're using 16bit uints because we're testing against std's u128 integer (which in turn can fit up to 8 pages at once, making it
-/// perfect for testing)
-type BitPage = u16;
-
-#[cfg(not(test))]
-/// In development though, it makes more sense to use larger pages, like 64bit uints
 type BitPage = u64;
 
 /// The amount of bits in our bit page
@@ -35,13 +25,14 @@ impl BitSet {
     }
 
     /// Set a bit at the provided location
-    pub fn set(&mut self, index: usize, to: bool) {
-        let value = self.get(index);
+    pub fn set(&mut self, bind: usize, to: bool) {
+        let value = self.get(bind);
 
-        let offset = index % PAGE_BITS;
-
+        let ind = bind / PAGE_BITS;
+        let offset = bind % PAGE_BITS;
+        
         if value != to {
-            self.pages[index] ^= 1 << offset; 
+            self.pages[ind] ^= 1 << ((PAGE_BITS-1)-offset); 
         }
     }
 
@@ -55,14 +46,19 @@ impl BitSet {
     }
 
     /// Get the value of the provided bit (indexed by bit index)
-    pub fn get(&mut self, index: usize) -> bool {
+    pub fn get(&self, index: usize) -> bool {
         assert!(index/PAGE_BITS < self.len);
 
         let offset = index % PAGE_BITS;
 
         (
-            self.pages[index/PAGE_BITS] & (1 << offset)
+            self.pages[index/PAGE_BITS] & (1 << ((PAGE_BITS-1)-offset))
         ) > 0
+    }
+
+    /// The length of this bitset in bits
+    pub fn bit_len(&self) -> usize {
+        self.len * PAGE_BITS
     }
 
     /// Shift this structure to the right
@@ -116,46 +112,6 @@ impl BitSet {
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use crate::window::{BitPage, BitSet, PAGE_BITS};
-
-    #[test]
-    fn test_bitset() {
-        const TEST_PAGES: usize = (u128::BITS as usize) / PAGE_BITS;
-
-        // Load our structure
-        let o_page: u128 = 0xF4F1748182F917A1293FA11283;
-        let o_bitset = {   
-            let mut bs = BitSet::new(TEST_PAGES);
-
-            // Load page by page our enormous u128 bitset
-            for ind in 0..TEST_PAGES {
-                bs.put(ind, (o_page >> ((TEST_PAGES-1)-ind)*PAGE_BITS) as BitPage);
-            }
-
-            bs
-        };
-
-        // For every shift amount
-        for shift_by in [1, 2, 3, 7, 50, 90, 124] {
-            
-            // Shift our 2 structures
-            let page = o_page.unbounded_shr(shift_by);
-            let mut bitset = o_bitset.clone();
-            bitset.shr(shift_by as _);
-
-            // Then compare them page by page
-            for ind in 0..TEST_PAGES {
-                let page_a = bitset.read(ind);
-                let page_b = (page >> ((TEST_PAGES-1)-ind)*PAGE_BITS) as BitPage;
-
-                assert_eq!(page_a, page_b);
-            }
-        }
-    }
-}
-
 /// The sliding window structure that helps keeping track of acknowledged packets.
 /// 
 /// The way it works, is the window contains the general offset (the latest packet), and frames that go **before** this offset packet. It can be
@@ -174,15 +130,32 @@ pub struct SlidingAckWindow {
     frames_amount: usize,
 
     /// The frame storage itself (has a constant size)
-    frames: Box<[u64]>
+    frames: BitSet
+}
+
+/// The mark of a packet, which describes its status in the packet window
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum PacketMark {
+    /// The packet is new (out of window bounds)
+    New,
+
+    /// The packet is within the bounds (marked)
+    Marked,
+
+    /// The packet is within the bounds (not marked)
+    NonMarked,
+
+    /// THe packet is old (out of bounds)
+    Old
 }
 
 impl SlidingAckWindow {
+
     /// Create a new sliding window with the provided amount of frames. 
     /// 
     /// Note that a single frame contains multiple packets (64 to be precise)
     pub fn new(frames_amount: usize) -> Self {
-        let frames = vec![0; frames_amount].into_boxed_slice();
+        let frames = BitSet::new(frames_amount);
 
         Self { 
             latest: None, 
@@ -191,21 +164,194 @@ impl SlidingAckWindow {
         }
     }
 
+    /// Mark this packet
     pub fn mark(&mut self, packet: PacketSeqId) {
         
         match self.latest {
+            // We don't have any packets, so this one is the first we encounter
             None => {
                 self.latest = Some(packet);
+                // Our first bit is then marked as our most recent packet
+                self.frames.set(0, true);
             },
+
+            // We already have a packet, so we must test against it
             Some(latest) => {
+
+                // If our packet is more recent than the latest - it automatically shifts our entire structure
                 if packet > latest {
+
+                    // Compute the delta
                     let delta = packet-latest;
                     
+                    // Shift our bits to the right
+                    self.frames.shr(delta as usize);
+
+                    // Mark this new packet right at the top
+                    self.frames.set(0, true);
+
+                    // This packet now is the latest one
+                    self.latest = Some(packet);
+                } else {
+                    // In any other case the packet is probably in our window
+
+                    // Compute its bit-index
+                    let bind = (latest-packet) as usize;
+
+                    // If it's in our window range - mark it
+                    if bind < self.frames.bit_len() {
+                        self.frames.set(bind, true);
+                    }
                 }
             }
         }
+
         if self.latest.is_none() {
             self.latest = Some(packet);
+        }
+    }
+
+    /// Get the mark status for the provided packet
+    pub fn get_marked(&self, packet: PacketSeqId) -> PacketMark {
+        match self.latest {
+            // We don't have any data whatsoever, so the packet is new
+            None => PacketMark::New,
+
+            Some(latest) => {
+
+                if packet > latest {
+                    // The packet is newer than our latest one
+                    PacketMark::New
+                } else {
+                    // In any other case we must compute a delta between these packets
+                    let delta = (latest-packet) as usize;
+                    println!("Delta: {delta}");
+
+                    if delta < self.frames.bit_len() {
+                        // Check if it's marked
+                        let marked = self.frames.get(delta);
+                        if marked {
+                            println!("Marked: {marked}");
+                            PacketMark::Marked
+                        } else {
+                            PacketMark::NonMarked
+                        }
+                    } else {
+                        // In any other case it's out of reach, so it probably was marked, but no longer
+                        PacketMark::Old
+                    }
+                }
+            }
+        }
+    }
+
+    /// The packet is considered "new" if it's newer or wasn't marked within the window bounds
+    pub fn is_new(&self, packet: PacketSeqId) -> bool {        
+        let mark = self.get_marked(packet);
+
+        // Our packet is new if it's old or non-marked (within window bounds)        
+        mark == PacketMark::New || mark == PacketMark::NonMarked
+    }
+
+    /// Check if this packet is old (no longer within the window bounds)
+    pub fn is_old(&self, packet: PacketSeqId) -> bool {
+        self.get_marked(packet) == PacketMark::Old
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::window::{BitPage, BitSet, PAGE_BITS, SlidingAckWindow};
+
+
+    #[test]
+    fn test_bitset_set() {
+        let mut bitset = BitSet::new(4);
+
+        for ind in 0..16 {
+            bitset.set(ind, true);
+            assert_eq!(bitset.get(ind), true);
+        }
+
+        bitset.shr(2);
+
+        assert_eq!(bitset.get(0), false);
+        assert_eq!(bitset.get(1), false);
+
+        assert_eq!(bitset.get(3), true);
+    }
+
+    #[test]
+    fn test_bitset_shift() {
+        const TEST_PAGES: usize = (u128::BITS as usize) / PAGE_BITS;
+
+        // Load our structure
+        let o_page: u128 = 0xF4F1748182F917A1293FA11283;
+        let o_bitset = {   
+            let mut bs = BitSet::new(TEST_PAGES);
+
+            // Load page by page our enormous u128 bitset
+            for ind in 0..TEST_PAGES {
+                bs.put(ind, (o_page >> ((TEST_PAGES-1)-ind)*PAGE_BITS) as BitPage);
+            }
+
+            bs
+        };
+
+        // For every shift amount
+        for shift_by in [1, 2, 3, 7, 50, 90, 129] {
+
+            // Shift our 2 structures
+            let page = o_page.unbounded_shr(shift_by);
+            let mut bitset = o_bitset.clone();
+            bitset.shr(shift_by as _);
+
+            // Then compare them page by page
+            for ind in 0..TEST_PAGES {
+                let page_a = bitset.read(ind);
+                let page_b = (page >> ((TEST_PAGES-1)-ind)*PAGE_BITS) as BitPage;
+
+                assert_eq!(page_a, page_b);
+            }
+        }
+    }
+
+    #[test]
+    fn test_ack_window() {
+        let mut window = SlidingAckWindow::new(2);
+
+        // Make a zero packet. It's not yet marked
+        let a = 0;
+        assert!(window.is_new(a));
+
+        // Mark it
+        window.mark(a);
+        assert!(!window.is_new(a));
+
+        // Let's mark some more packets
+        for p in 1..16 {
+            assert!(window.is_new(p));
+            window.mark(p);
+
+            assert!(!window.is_new(p));
+        }
+
+        // Now we're going to mark 128, which will destroy the oldest packet we got (0)
+        window.mark(128);
+
+        assert!(window.is_old(a));
+
+        // However, all the other packets must still be present
+        for p in 1..16 {
+            assert!(!window.is_new(p));
+        }
+
+        // Let's mark a more recent one
+        window.mark(144);
+
+        // These now should be obsolete
+        for p in 1..16 {
+            assert!(window.is_old(p));
         }
     }
 }
